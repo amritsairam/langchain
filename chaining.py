7c0d616cae662fc3b78da4df6a86496a326ca35a
amritsairam <124433494+amritsairam@users.noreply.github.com>
2024-02-03 13:31:25 +0530

    chaining and prompt engineering

--- chaining.py
+++ chaining.py
@@ -1,2 +1,11 @@
-import openai
-import langchain
+import os 
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+from langchain.prompts import PromptTemplate
+
+prompt_template = PromptTemplate.from_template(
+    "you are an expert financial advisor, give the user more information about {topic}"
+)
+prompt_template.format(adjective="funny", content="chickens")
